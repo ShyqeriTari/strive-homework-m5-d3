ffa08d5cd4e87bcee7efe03975058d69c228fbad
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-01 16:03:40 +0100

    Fetching data

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -3,6 +3,28 @@ import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
 export default class BlogList extends Component {
+
+state = {undefined}
+
+fetchData = async() => {
+  try {
+
+    const response = await fetch("http://localhost:3001/authors")
+    const data = await response.json()
+    
+if (response.ok){
+    this.setState(data)
+    console.log(data)
+}
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+componentDidMount = () => {
+  this.fetchData()
+}
+
   render() {
     return (
       <Row>
